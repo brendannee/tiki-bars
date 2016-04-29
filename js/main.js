99198a9748a8ec1a6aef4108e9281900410d6903
Brendan Nee <brendan@automatic.com>
2016-04-29 21:45:50 +0200

    https

--- js/main.js
+++ js/main.js
@@ -12,7 +12,7 @@ var tikiBars = [
     address: '650 Gough St, San Francisco, CA',
     description: 'World\'s smallest three-story bar',
     website: 'http://http://smugglerscovesf.com/',
-    lat: 37.77941, 
+    lat: 37.77941,
     lng: -122.42335
   },
   {
@@ -20,7 +20,7 @@ var tikiBars = [
     address: '6150 Geary Blvd, San Francisco, CA',
     description: 'Divey, old-time tiki',
     website: 'http://critiki.com/location/?loc_id=157',
-    lat: 37.78018, 
+    lat: 37.78018,
     lng: -122.48565
   },
   {
@@ -28,7 +28,7 @@ var tikiBars = [
     address: '9 Anchor Dr, Emeryville, CA',
     description: 'A real tiki bar out on the Emeryville Marina.',
     website: 'http://tradervicsemeryville.com',
-    lat: 37.8380, 
+    lat: 37.8380,
     lng: -122.3078
   },
   {
@@ -36,7 +36,7 @@ var tikiBars = [
     address: '5422 College Ave, Oakland, CA',
     description: 'Upstairs Tiki Bar in Rockridge',
     website: 'http://congalounge.com',
-    lat: 37.84069, 
+    lat: 37.84069,
     lng: -122.25127
   },
   {
@@ -44,15 +44,15 @@ var tikiBars = [
     address: '1304 Lincoln Ave, Alameda, CA',
     description: 'Island-themed Tiki bar on a real Island.',
     website: 'http://forbiddenislandalameda.com',
-    lat: 37.77460, 
-    lng: -122.26291 
+    lat: 37.77460,
+    lng: -122.26291
   },
   {
     name: 'Bamboo Hut',
     address: '479 Broadway, San Francisco, CA',
     description: '',
     website: 'http://critiki.com/location/?loc_id=158',
-    lat: 37.797950, 
+    lat: 37.797950,
     lng: -122.405071
   },
   {
@@ -60,7 +60,7 @@ var tikiBars = [
     address: '1334 Noriega St, San Francisco, CA',
     description: '',
     website: 'http://critiki.com/location/?loc_id=784',
-    lat: 37.75414, 
+    lat: 37.75414,
     lng: -122.47821
   },
   {
@@ -68,15 +68,15 @@ var tikiBars = [
     address: '4401 Piedmont Ave, Oakland, CA',
     description: '',
     website: 'http://critiki.com/location/?loc_id=543',
-    lat: 37.83040, 
-    lng: -122.24732 
+    lat: 37.83040,
+    lng: -122.24732
   },
   {
     name: 'Hukilau',
     address: '230 Jackson Street, San Jose, CA',
     description: '',
     website: 'http://www.dahukilau.com/',
-    lat: 37.34895, 
+    lat: 37.34895,
     lng: -121.8935
   },
   {
@@ -84,17 +84,17 @@ var tikiBars = [
     address: '144 South B St., San Mateo, CA',
     description: '',
     website: 'http://www.thedowntowntikilounge.com/',
-    lat: 37.56651, 
+    lat: 37.56651,
     lng: -122.32365
   },
 ];
 
-var mbTiles = new L.tileLayer('http://tiki.bn.ee/php/mbtiles.php?z={z}&x={x}&y={y}', {
+var mbTiles = new L.tileLayer('https://tiki.bn.ee/php/mbtiles.php?z={z}&x={x}&y={y}', {
     tms: true,
-    attribution: 'Tiles Courtesy of <a href="http://tiles.mapbox.com/mapbox/map/geography-class" target="_blank">MapBox</a>',
+    attribution: 'Tiles Courtesy of <a href="https://tiles.mapbox.com/mapbox/map/geography-class" target="_blank">MapBox</a>',
 });
 
-var mapboxFallback = new L.TileLayer('http://a.tiles.mapbox.com/v3/brendannee.map-28wp10x1/{z}/{x}/{y}.png');
+var mapboxFallback = new L.TileLayer('https://a.tiles.mapbox.com/v3/brendannee.map-28wp10x1/{z}/{x}/{y}.png');
 
 var map = new L.Map('map', {layers: [mapboxFallback, mbTiles], zoom: 12, center: [37.7255, -122.3186], maxZoom: 15, minZoom: 10});
 
@@ -117,7 +117,7 @@ tikiBars.forEach(function(bar, idx){
 
 if (window.HTMLAudioElement) {
   var snd = new Audio('');
-   
+
   if(snd.canPlayType('audio/ogg')) {
     snd = new Audio('sound/tiki.ogg');
   }
@@ -161,4 +161,3 @@ window.setTimeout(function(){
   flyPlane();
   window.setInterval(flyPlane, 77000);
 }, 28000)
-
